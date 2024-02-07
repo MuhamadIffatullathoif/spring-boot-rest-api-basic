a0d0f127011851ee25677b321cb704abf396e041
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-07 13:58:11 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springbootrestapisecond/SpringBootRestApiSecondApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springbootrestapisecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiSecondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iffat/springbootrestapisecond/SpringBootRestApiSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.springbootrestapisecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
